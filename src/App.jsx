ebfdd6525cf85f2951d7842eec57a979a6bf5817
Akshit Gupta <akshit0689@gmail.com>
2024-03-23 15:15:43 +0530

    Dividing the app into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-function App() {
 
-  return <center className = 'todo-container'><h1>ToDo App</h1>
+import AppName from "./components/AppName";
+
+import AddToDO from "./components/AddToDo";
+
+function App() {
+  return <center className = 'todo-container'><AppName />
   
   <div class="container text-center">
-  <div class="row">
-    <div class="col-6"><input type="text" placeholder="Enter To-do here" /></div>
-    <div class="col-4"><input type="date" placeholder="Enter date" /></div>
-    <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
-  </div>
+  <AddToDO />
+  <AddToDO />
 
   <div class="row">
     <div class="col-6">Buy Milk</div>
---
+++ src/components/AddToDo.jsx
@@ -0,0 +1,14 @@
+function AddToDO(){
+
+  return (
+    <div class="container text-center">
+  <div class="row">
+    <div class="col-6"><input type="text" placeholder="Enter To-do here" /></div>
+    <div class="col-4"><input type="date" placeholder="Enter date" /></div>
+    <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
+  </div>
+  </div>
+  )
+}
+
+export default AddToDO;
