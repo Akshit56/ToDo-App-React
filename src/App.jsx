86758abec84b7e16bf60ddf9e4994fc2eb9c2371
Akshit Gupta <akshit0689@gmail.com>
2024-03-23 17:23:57 +0530

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-
 import AppName from "./components/AppName";
-
 import AddToDO from "./components/AddToDo";
-
 import ToDoItem1 from "./components/ToDoItem1";
-
 import ToDoItem2 from "./components/ToDoItem2";
+import "./App.css";
 
 function App() {
-  return <center className = 'todo-container'><AppName />
-  
-  <div class="container text-center">
+  return (
+  <center className = 'todo-container'>
+  <AppName />
   <AddToDO />
   <AddToDO />
-
   <ToDoItem1 />
   <ToDoItem2 />
-
-  
-
-</div>
-  
   </center>
+  );
 }
+
 export default App
--- src/components/ToDoItem2.jsx
+++ src/components/ToDoItem2.jsx
@@ -1,12 +1,21 @@
 function ToDoItem2(){
 
+  let todoName = 'Go to college';
+  let todoDate = '4/10/2023'
 
   return (
+    <div class="container text-center">
+
     <div class="row">
-    <div class="col-6">Go to college</div>
-    <div class="col-4">4/10/2023</div>
-    <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
-  </div>
+    <div class="col-6">{todoName}</div>
+    <div class="col-4">{todoDate}</div>
+    <div class="col-2">
+    <button type="button" class="btn btn-danger">Delete
+    </button>
+    </div>
+    </div>
+    </div>
+    
   )
 }
 
