e26f0a107b8deb68f0ce709bb9026511a7e86946
Akshit Gupta <akshit0689@gmail.com>
2024-03-23 15:06:16 +0530

    basic to do features and ui using bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 function App() {
 
-  return <div>placeholder</div>
+  return <center className = 'todo-container'><h1>ToDo App</h1>
+  
+  <div class="container text-center">
+  <div class="row">
+    <div class="col-6"><input type="text" placeholder="Enter To-do here" /></div>
+    <div class="col-4"><input type="date" placeholder="Enter date" /></div>
+    <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
+  </div>
+
+  <div class="row">
+    <div class="col-6">Buy Milk</div>
+    <div class="col-4">15/10/2023</div>
+    <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+  </div>
+
+  <div class="row">
+    <div class="col-6">Go to college</div>
+    <div class="col-4">4/10/2023</div>
+    <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+  </div>
+
+</div>
+  
+  </center>
 }
 export default App
