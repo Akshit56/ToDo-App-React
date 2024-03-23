50ffbc2423b984f66777ca7234b83428c086b855
Akshit Gupta <akshit0689@gmail.com>
2024-03-23 15:20:38 +0530

    defining components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import AppName from "./components/AppName";
 
 import AddToDO from "./components/AddToDo";
 
+import ToDoItem1 from "./components/ToDoItem1";
+
+import ToDoItem2 from "./components/ToDoItem2";
+
 function App() {
   return <center className = 'todo-container'><AppName />
   
@@ -10,17 +14,10 @@ function App() {
   <AddToDO />
   <AddToDO />
 
-  <div class="row">
-    <div class="col-6">Buy Milk</div>
-    <div class="col-4">15/10/2023</div>
-    <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
-  </div>
+  <ToDoItem1 />
+  <ToDoItem2 />
 
-  <div class="row">
-    <div class="col-6">Go to college</div>
-    <div class="col-4">4/10/2023</div>
-    <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
-  </div>
+  
 
 </div>
   
---
+++ src/components/ToDoItem1.jsx
@@ -0,0 +1,13 @@
+function ToDoItem1(){
+
+  return (
+    <div class="row">
+    <div class="col-6">Buy Milk</div>
+    <div class="col-4">15/10/2023</div>
+    <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+  </div>
+
+  )
+}
+
+export default ToDoItem1;
